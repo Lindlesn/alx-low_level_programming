003d08d48a8b2b54b1142a2ebb2c705146e69d43
Lindlesn <lindesignsza@gmail.com>
2023-02-16 17:28:51 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /*
  * main - entry point
  *
- * Return : 0
+ * Return : Always 0
  */
-
-int main(void){
-	puts ("Programming is like building a multilingual puzzle")
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle")
 	return (0);
 }
