5101e5480609cb8d7c820e31729d1e9e5a3e9901
Lindlesn <lindesignsza@gmail.com>
2023-02-28 15:07:44 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string length to be calculated
+ *
+ * Return: Length of a string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}
+
