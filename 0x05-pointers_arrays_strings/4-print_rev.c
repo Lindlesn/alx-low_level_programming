e9120d97de03a829ebf123b041007fed7234d6cf
Lindlesn <lindesignsza@gmail.com>
2023-02-28 16:01:57 -0800

    4-print_rev.c fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,12 +12,12 @@ void print_rev(char *s)
 	int i = 0;
 	int length;
 
-	for (lenth = 0; s[length] != '\0'; i++)
+	for (length = 0; s[length] != '\0'; i++)
 	{
 	}
 	for (i = length -1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
