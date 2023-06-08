15c7e00a2e23c968c049973ee7ce7de7985a6d88
Lindlesn <lindesignsza@gmail.com>
2023-06-08 02:18:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char phrase[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(phrase);
+
+	return (0);
+}
