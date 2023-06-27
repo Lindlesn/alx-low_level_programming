d88fbded098151ab5a83350abf053ec69d727b0d
Lindlesn <lindesignsza@gmail.com>
2023-06-27 01:07:57 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: 0
+ *
+ */
+
+void print_alphabet(void)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
