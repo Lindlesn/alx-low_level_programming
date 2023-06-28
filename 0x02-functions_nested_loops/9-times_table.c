94848ebb0a6b501d4bad3bb01c47bc0bfa3f8237
Lindlesn <lindesignsza@gmail.com>
2023-06-28 01:45:55 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,18 +17,17 @@ void times_table(void)
 		{
 			int k = i * j;
 
-			if (k <= 9)
+			if (k <= 9 && j != 9)
 			{
 				_putchar(k + '0');
+				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 			}
-			else
+			else if (k > 9 && j != 9)
 			{
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
-			}
-			if (j != 9)
-			{
 				_putchar(',');
 				_putchar(' ');
 			}
