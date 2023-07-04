a499b611ac4b2611802330a334bc1858e8c01892
Lindlesn <lindesignsza@gmail.com>
2023-07-04 04:49:50 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * _atoi - converts a string to an integer
  *
  * @s: string to be converted
@@ -34,7 +33,7 @@ int _atoi(char *s)
 			}
 			n = n * 10 + num;
 			k = 1;
-			if (s[i +1] < '0' || s[i + 1] > '9')
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 			{
 				break;
 			}
