fdf1e2c362eb68875fe3d0d3d8270e19a0623820
Lindlesn <lindesignsza@gmail.com>
2023-07-04 03:37:51 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 - 9
+ *
+ * Return: nothing
+ *
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
