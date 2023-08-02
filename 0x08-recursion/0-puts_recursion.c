d6570ed39de354aeb2751c4987da5e004fecc1ff
Lindlesn <lindesignsza@gmail.com>
2023-08-02 11:46:02 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - recursive function to print a string
+ *
+ * @s: string to be printed
+ *
+ * Return: nothing
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
