d74a8b25e2e836a8ee6579a6e3cd6bb9bf0919f7
Lindlesn <lindesignsza@gmail.com>
2023-08-17 15:38:34 -0700

    betty fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,7 +7,7 @@
  * @size: size of array
  * @cmp: pointer to the function to be used to compsre values
  *
- * Return:  index of the first element 
+ * Return: index of the first element
  * for which the cmp function does not return 0
  * If no element matches, return -1
  * If size <= 0, return -1
